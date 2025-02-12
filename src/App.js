09e4fc4430f3969f9d83d70d44c9d2c759bc440f
samnodier <sam0132nodier@gmail.com>
2025-02-12 18:48:29 -0500

    final community for week 2 assignment

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import logo from './logo.svg';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+// import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages/Home';
-import Contact from './pages/Contact';
+// import Contact from './pages/Contact';
 import './styles/App.css';
 
 function App() {
   return (
-    <Router>
     <div className="App">
       <nav>
         <img src={logo} className="App-logo" alt="logo" />
         <div>
-        <a href="/">Home</a>
-        <a href="/contact">Contact</a>
+        <a href="#">Home</a>
+        <a href="#">Contact</a>
         </div>
       </nav>
-      <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="/contact" element={<Contact />}/>
-
-      </Routes>
-    </div>
-    </Router>
+      <Home />
+</div>
+ 
   );
 }
 
