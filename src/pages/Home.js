5d773fce2d1ce5e2a5caf78503d8799e3a57266b
samnodier <sam0132nodier@gmail.com>
2025-02-12 05:57:07 -0500

    the week 2 assignment final version

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+    return(
+    <div>
+        <h1>Welcome to The Cozy Corner</h1>
+        <p>Your site is published at ...</p>
+    </div>
+    );
+}
+
+export default Home;
