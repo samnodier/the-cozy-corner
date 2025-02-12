ad0483aa80c75153c4fd47f158a2120da3d3ea68
samnodier <sam0132nodier@gmail.com>
2025-02-12 05:59:08 -0500

    the week 2 assignment final version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import './styles/App.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
+      <nav>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+        <a href="/">Home</a>
+        <a href="/contact">Contact</a>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/contact" element={<Contact />}/>
+
+      </Routes>
     </div>
+    </Router>
   );
 }
 
